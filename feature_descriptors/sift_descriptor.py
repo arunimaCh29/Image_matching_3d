536e938914f3e39f215345d5198b63a331d79f7c
kezia nugroho <kezia.nugroho@mtd-solutions.com>
2025-06-20 14:22:26 +0200

    Add parameter sift_nfeatures

--- feature_descriptors/sift_descriptor.py
+++ feature_descriptors/sift_descriptor.py
@@ -2,12 +2,13 @@ import numpy as np
 import torch
 import cv2 as cv
 
-def get_keypoint_and_descriptor(image):
+def get_keypoint_and_descriptor(image, sift_nfeatures=None):
     """
     Get the keypoint and descriptor of the image
 
     Args:
-        image (tensor or numpy.ndarray): input image, should be at least as tensor (but doesn't need to be normalized) or numpy array
+        image (tensor or numpy.ndarray) : input image, should be at least as tensor (but doesn't need to be normalized ) or numpy array
+        sift_nfeatures : maximum features for SIFT
     
     Returns:
         numpy.ndarray   : image in grayscale as numpy array
@@ -30,7 +31,7 @@ def get_keypoint_and_descriptor(image):
         img_np = cv.cvtColor(img_np, cv.COLOR_RGB2GRAY)
 
     # Create SIFT
-    sift = cv.SIFT_create()
+    sift = cv.SIFT_create(nfeatures=sift_nfeatures) if sift_nfeatures else cv.SIFT_create()
     # Compute the image's keypoint and descriptor
     keypoint, descriptor = sift.detectAndCompute(img_np, None)
 
